104adb5b1bcefc964d0bd983de117d02b44f79f1
Gabriel Barroso <biel.barroso@hotmail.com>
2025-04-08 20:22:51 -0300

    exercicio_final

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,26 @@
 import MovieList from "./components/MovieList";
+import MovieCard from "./components/MovieCard";
+import { useState } from "react";
 
 const App = () => {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const handleMovieSelect = (movieId) => {
+    setSelectedMovie(movieId);
+  }
+
+  const handleBacktoList = () => {
+    setSelectedMovie(null);
+  }
+
+  if (selectedMovie) {
+    return (
+      <div>
+        <h1>Catálogo de Filmes</h1>
+        <MovieCard movieId={selectedMovie} onBack={handleBacktoList} />
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Catálogo de Filmes</h1>
--- react/src/components/MovieCard.jsx
+++ react/src/components/MovieCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const MovieCard = ({ title, description, poster, rating, year }) => {
+const MovieCard = ({ title, description, poster, rating, year, OnSelectMovie }) => {
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={() => OnSelectMovie(id)}>
       <img src={poster} alt={title} />
       <h3>{title} {year && `(${year})`}</h3>
       <p>{description}</p>
